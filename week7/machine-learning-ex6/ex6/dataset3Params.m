426403e28c0991a3a092d3bd7ad5ae740d3fb039
Leticia <leticia.campos.cm@gmail.com>
2019-10-10 20:41:58 +0200

    Add solution assignment week7

--- week7/machine-learning-ex6/ex6/dataset3Params.m
+++ week7/machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,29 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+increments = [ 0.01, 0.03, 0.1, 0.3, 1.0, 3.0, 10.0, 30.0];
+incrementC = increments;
+incrementSigma = increments;
 
+best = zeros(3,1);
+trialNum = 1;
+for tryC = incrementC
+	for tryS = incrementSigma
+		fprintf(['Attempt #%d: C = %f, sigma = %f\n'], trialNum, tryC, tryS);
+		trialNum = trialNum + 1;
+		model = svmTrain(X, y, tryC, @(x1, x2) gaussianKernel(x1, x2, tryS));
+		prediction = svmPredict(model, Xval);
+		predError = mean(double(prediction == yval));
+		if(predError > best(1))
+			fprintf(['Best so far %f\n'], predError);
+			best = [predError, tryC, tryS];
+		end
+	end
+end
 
-
-
-
+fprintf(['\n Best Found: C = %f, sigma = %f\n'], best(2), best(3));
+C = best(2);
+sigma = best(3);
 
 % =========================================================================
 
--- week7/machine-learning-ex6/ex6/processEmail.m
+++ week7/machine-learning-ex6/ex6/processEmail.m
@@ -96,16 +96,13 @@ while ~isempty(email_contents)
     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
     %       str2). It will return 1 only if the two strings are equivalent.
     %
-
-
-
-
-
-
-
-
-
-
+    
+    idx = find(strcmp(vocabList, str));
+    
+    if(length(idx) == 1)
+        word_indices = [word_indices; idx];
+    end
+    
     % =============================================================
 
 
